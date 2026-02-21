5207ff4e94a08c89de588eece30c3291f7b07dbf
olsanju-hub <olsanju@gmail.com>
2026-02-22 00:15:03 +0100

    Update session.js

--- js/session.js
+++ js/session.js
@@ -8,4 +8,4 @@ window.HK_ALGO_SRC = "assets/images/algo/algoritmo.png";
 
 // ECG
 window.HK_ECG_MAX   = 10; // máximo “permitido” por diseño
-window.HK_ECG_TOTAL = 4;  // los que tienes HOY (hk_001..hk_004). Sube a 10 cuando existan.
+window.HK_ECG_TOTAL = 5;  // los que tienes HOY (hk_001..hk_004). Sube a 10 cuando existan.
